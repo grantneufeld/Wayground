ab7224dbcd7ce8bd40246b788fdd790939c7f037
Grant Neufeld <grant@grantneufeld.ca>
2010-12-29 13:52:43 -0700

    New Rails 3.0.3 app.
     > rails new wayground
    
    Plus a few modifications:
    * Added some additional filenames/matches to .gitignore.
    * Indentation converted from the default multi-space to my preferred tabs.
    * Removed default files that won’t be used (doc/README_FOR_APP, public/index.html).
    * Changed README to README.markdown.
    * Copy config/database.yml to config/database.default.yml.
    * Hide config/initializers/secret_token.rb from git and add a secret_token.default.rb with instructions on creating the secret_token.rb file.
    
    Initialization of the empty database:
     > rake db:migrate
     > rake db:test:prepare
    
    Creation of git repository:
     > git init

---
+++ config/initializers/secret_token.default.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wayground::Application.config.secret_token = '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+
+# IMPORTANT: COPY THIS FILE TO secret_token.rb AND EDIT THE LINE OF ZEROS TO BE AN ACTUAL SECRET TOKEN (e.g., a random hex value)
