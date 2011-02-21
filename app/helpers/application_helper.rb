0ca53ad72a29e0970a0f9fa014fb56fc39eeb791
Grant Neufeld <grant@grantneufeld.ca>
2011-02-21 10:41:39 -0700

    General application layout and form errors reporting.
    Application Layout:
    - More details in head, including specifying utf-8 charset.
    - Favicon link.
    - Block for displaying flash messages.
    - Div (id: “content”) wrapper for the main content.
    
    Wayground logo favicon.
    
    Error reporting for forms (uses a helper and partial).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def show_errors(item, heading=nil)
+		render :partial => 'layouts/errors', :locals => {:item => item, :heading => heading}
+	end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ApplicationHelper. For example:
+#	describe ApplicationHelper do
+#		describe "string concat" do
+#			it "concats two strings with spaces" do
+#				helper.concat_strings("this","that").should == "this that"
+#			end
+#		end
+#	end
+describe ApplicationHelper do
+	describe "#show_errors" do
+		it "displays errors (ActiveRecord-style) from an object" do
+			item = User.new({:email => 'invalid'})
+			item.valid?
+			helper.show_errors(item).should match /Errors encountered/i
+		end
+	end
+end
