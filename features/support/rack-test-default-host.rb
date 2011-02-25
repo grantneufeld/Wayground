8ff68690f9f42a8f2244bfc514fb785d18fd76f7
Grant Neufeld <grant@grantneufeld.ca>
2011-02-24 19:28:51 -0700

    Patch for getting rack in line with webrat for test host name.
    Problem described at:
    http://baldowl.github.com/2010/12/06/coercing-cucumber-and-webrat-to-cooperate.html
    
    Fix from:
    https://gist.github.com/729996
    
    I ran into this while trying to run a cucumber feature that involved a controller redirecting the browser.

---
+++ features/support/rack-test-default-host.rb
@@ -0,0 +1,17 @@
+# Necessary evil thing: Rack::Test sports as default host
+# "example.org", but Webrat and Ruby on Rails's integration test
+# classes use "example.com"; this discrepancy leads to Webrat not
+# being able to follow simple internal redirects.
+#
+# Drop in in features/support/
+
+module Rack
+	module Test
+		DEFAULT_HOST = "example.com"
+	end
+end
+
+# Source:
+# https://gist.github.com/729996
+# Described at:
+# http://baldowl.github.com/2010/12/06/coercing-cucumber-and-webrat-to-cooperate.html
