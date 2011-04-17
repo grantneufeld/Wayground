2da819b951b287f4542a7cca56f461db7822b93a
Grant Neufeld <grant@grantneufeld.ca>
2011-04-16 19:19:32 -0600

    WYSIWYG html editing for page content, via CKEditor 3.5.3.
    CKEditor is an open-source JavaScript wysiwyg html editor.
    http://ckeditor.com/

---
+++ public/ckeditor/config.js
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	config.bodyClass = 'wysiwygeditor';
+	config.coreStyles_bold = { element : 'strong', overrides : 'b' };
+	config.coreStyles_italic = { element : 'em', overrides : 'i' };
+
+	config.contentsCss = ['/stylesheets/wayground-all.css','/stylesheets/wayground-screen.css'];
+	config.docType = '<!DOCTYPE html>';
+
+	config.disableNativeTableHandles = false;
+	
+	config.stylesSet = 'wayground:/javascripts/ckeditor_styles.js';
+	config.toolbar = 'WaygroundToolbar';
+	config.toolbar_WaygroundToolbar =
+	[
+	['Maximize','ShowBlocks','-','Source','-','Preview'], //,'-','Templates'],
+	['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker','Scayt'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	//['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+	'/',
+	['Format','-','Blockquote','-','NumberedList','BulletedList'],
+	['Image','Flash','Table','SpecialChar','PageBreak'],
+	'/',
+	['Link','Unlink','Anchor'],
+	['Styles'],
+	['Bold','Italic','Strike','-','Subscript','Superscript'],
+	['About']
+	];
+};
