367fb9908b36fcb44b361dffc0abc4e168a6b477
Grant Neufeld <grant@grantneufeld.ca>
2013-02-17 16:44:57 -0700

    [CookieToken] format token strings for use in cookies.

---
+++ app/wayground/cookie_token.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+module Wayground
+
+  # Turns a token for an object to remember (i.e., User) into a string usable in cookies.
+  # Usage: CookieToken.new(remember: user, token: token_string).to_s
+  class CookieToken
+    attr_accessor :id, :token
+
+    def initialize(params={})
+      self.id = params[:remember].id
+      self.token = params[:token]
+    end
+
+    # Return a string with the token and id of the object to remember, for use in cookies.
+    def to_s
+      "#{token}/#{id}"
+    end
+  end
+
+end
