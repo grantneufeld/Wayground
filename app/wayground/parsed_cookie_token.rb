0e2483c492e8a9f94299edee99bddcb4bb5b5437
Grant Neufeld <grant@grantneufeld.ca>
2013-02-17 16:50:34 -0700

    [ParsedCookieToken] parse a token string, formatted for a cookie, into token and id.

---
+++ app/wayground/parsed_cookie_token.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+module Wayground
+
+  # Takes a cookie token string (“token/id”) and extracts the “id” and “token”.
+  # You can then use those to find a RememberToken for a given object (by id).
+  # Usage:
+  #   user = User.find(ParsedCookieToken.new(cookie_token).id)
+  #   token = ParsedCookieToken.new(cookie_token).token
+  class ParsedCookieToken
+    attr_accessor :token, :id
+
+    # The supplied token string does not match the required cookie token format.
+    class InvalidToken < Exception; end
+
+    def initialize(cookie_token)
+      raise InvalidToken unless cookie_token
+      token_parsed = cookie_token.match(/\A(?<token>.+)\/(?<id>[0-9]+)\z/)
+      raise InvalidToken unless token_parsed
+      self.token = token_parsed[:token]
+      self.id = token_parsed[:id].to_i
+    end
+  end
+
+end
---
+++ spec/wayground/parsed_cookie_token_spec.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'parsed_cookie_token'
+
+describe Wayground::ParsedCookieToken do
+
+  describe "initialization" do
+    it "should parse a valid cookie token into token and id" do
+      parsed_token = Wayground::ParsedCookieToken.new('token-456/4567')
+      expect( parsed_token.instance_values ).to eq({'id' => 4567, 'token' => 'token-456'})
+    end
+    it "should raise an error on an invalid cookie token" do
+      expect{
+        Wayground::ParsedCookieToken.new('invalid token')
+      }.to raise_exception(Wayground::ParsedCookieToken::InvalidToken)
+    end
+  end
+
+  describe "#token" do
+    it "should return the token from the cookie token" do
+      expect( Wayground::ParsedCookieToken.new('the token/567').token ).to eq 'the token'
+    end
+  end
+
+  describe "#id" do
+    it "should return the id from the token as an integer" do
+      expect( Wayground::ParsedCookieToken.new('token-678/6789').id ).to eq 6789
+    end
+  end
+
+end
