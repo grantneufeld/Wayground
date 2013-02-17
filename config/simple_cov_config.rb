6f8ff64e6e3fa24948822cc716f7994dd01ecb5d
Grant Neufeld <grant@grantneufeld.ca>
2013-02-17 16:41:53 -0700

    Setup for `app/wayground` to be used for non-ActiveRecord models.

--- config/simple_cov_config.rb
+++ config/simple_cov_config.rb
@@ -2,11 +2,11 @@ require 'simplecov'
 SimpleCov.start do
   add_filter "/app/models/test_model.rb" # comment this line out if you want to test the testing system
   add_filter "/config/"
-  add_filter "/coverage/"
   add_filter "/db/"
   add_filter "/doc/"
   add_filter "/features/"
   add_filter "/public/"
+  add_filter "/reports/"
   add_filter "/spec/"
   add_filter "/test/"
   add_filter "/tmp/"
@@ -15,6 +15,7 @@ SimpleCov.start do
   add_group "Models", "app/models"
   add_group "Controllers", "app/controllers"
   add_group "Helpers", "app/helpers"
+  add_group "Wayground", "app/wayground"
   add_group "Libraries", "lib"
   #add_group "Plugins", "vendor/plugins"
   add_group "Long files" do |src_file|
