932492b2cb4f292cabc9e5a4dcd51dfe25ea0699
Grant Neufeld <grant@grantneufeld.ca>
2013-02-18 01:49:59 -0700

    [CryptedPassword] wrapper model for crypted password values.
    Used to check if a given plain-text password matches the crypted password.

---
+++ app/wayground/crypted_password.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+require 'bcrypt'
+
+module Wayground
+
+  # Wrapper for crypted password values.
+  class CryptedPassword
+    attr_accessor :crypted_password
+
+    # Initialize from a string or an existing CryptedPassword.
+    def initialize(crypted_pass)
+      self.crypted_password = crypted_pass
+    end
+
+    # Checks whether the given password string matches the one that was used
+    # to create the crypted password.
+    def ==(pass)
+      BCrypt::Password.new(crypted_password.to_s) == pass
+    end
+
+    def to_s
+      crypted_password.to_s
+    end
+  end
+
+end
---
+++ spec/wayground/crypted_password_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'crypted_password'
+require 'bcrypt'
+
+module Wayground
+
+  describe CryptedPassword do
+    describe "#initialize" do
+      it "should save the given crypted password" do
+        expect( CryptedPassword.new('crypted-pass').crypted_password ).to eq 'crypted-pass'
+      end
+    end
+
+    describe "#==" do
+      it "should return true when the string matches the one used to generate the crypted password" do
+        pass = 'test pass'
+        expect( (CryptedPassword.new(BCrypt::Password.create(pass)) == pass) ).to be_true
+      end
+      it "should return false when the string does not match the one used to generated the crypted password" do
+        expect( (CryptedPassword.new(BCrypt::Password.create('pass1')) == 'pass2') ).to be_false
+      end
+    end
+
+    describe "#to_s" do
+      it "should return the crypted pass string" do
+        expect( CryptedPassword.new('pass string').to_s ).to eq 'pass string'
+      end
+    end
+  end
+
+end
