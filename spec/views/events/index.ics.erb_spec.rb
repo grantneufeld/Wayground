be1bbb06626460211eb0f8dc25ccaa4e2c9e7357
Grant Neufeld <grant@grantneufeld.ca>
2013-05-05 20:02:08 -0600

    Event iCalendar views - new UID format.
    Changed the UID format because of SEQUENCE being out of sync from reseting the Versions on Events.
    
    Also updated to ruby 1.9 hash syntax for affected files.

--- spec/views/events/index.ics.erb_spec.rb
+++ spec/views/events/index.ics.erb_spec.rb
@@ -8,34 +8,34 @@ describe "events/index.ics.erb" do
     @events = assign(:events,
       [
       FactoryGirl.create(:event,
-        :id => 123,
-        :start_at => Time.parse('2011-02-03 04:05:06 am MST'),
-        :is_allday => false,
-        :is_draft => false,
-        :is_approved => true,
-        :is_wheelchair_accessible => false,
-        :is_adults_only => false,
-        :is_tentative => false,
-        :is_cancelled => true,
-        :is_featured => false,
-        :title => "Event One",
-        :created_at => Time.parse('2001-02-03 04:05:06 am MST'),
-        :updated_at => Time.parse('2001-02-03 04:05:06 am UTC')
+        id: 123,
+        start_at: Time.parse('2011-02-03 04:05:06 am MST'),
+        is_allday: false,
+        is_draft: false,
+        is_approved: true,
+        is_wheelchair_accessible: false,
+        is_adults_only: false,
+        is_tentative: false,
+        is_cancelled: true,
+        is_featured: false,
+        title: "Event One",
+        created_at: Time.parse('2001-02-03 04:05:06 am MST'),
+        updated_at: Time.parse('2001-02-03 04:05:06 am UTC')
       ),
       FactoryGirl.create(:event,
-        :id => 234,
-        :start_at => Time.parse('2011-02-03 04:05:06 am MST'),
-        :is_allday => false,
-        :is_draft => false,
-        :is_approved => true,
-        :is_wheelchair_accessible => false,
-        :is_adults_only => false,
-        :is_tentative => true,
-        :is_cancelled => false,
-        :is_featured => false,
-        :title => "Event Two",
-        :created_at => Time.parse('2001-02-03 04:05:06 am MST'),
-        :updated_at => Time.parse('2001-02-03 04:05:06 am UTC')
+        id: 234,
+        start_at: Time.parse('2011-02-03 04:05:06 am MST'),
+        is_allday: false,
+        is_draft: false,
+        is_approved: true,
+        is_wheelchair_accessible: false,
+        is_adults_only: false,
+        is_tentative: true,
+        is_cancelled: false,
+        is_featured: false,
+        title: "Event Two",
+        created_at: Time.parse('2001-02-03 04:05:06 am MST'),
+        updated_at: Time.parse('2001-02-03 04:05:06 am UTC')
       )
       ]
     )
@@ -43,8 +43,8 @@ describe "events/index.ics.erb" do
 
   it "renders the expected text" do
     render
-    rendered.should match(
-    /\ABEGIN:VEVENT\r\nUID:123-event@wayground.ca\r\nCREATED:20010203T110506Z\r\nDTSTAMP:20010203T040506Z\r\nDTSTART;TZID=America\/Denver:20110203T040506\r\nSUMMARY:Event One\r\nCLASS:PUBLIC\r\nURL:http:\/\/[a-z0-9\.]+\/events\/123\r\nSTATUS:CANCELLED\r\nSEQUENCE:1\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:234-event@wayground.ca\r\nCREATED:20010203T110506Z\r\nDTSTAMP:20010203T040506Z\r\nDTSTART;TZID=America\/Denver:20110203T040506\r\nSUMMARY:Event Two\r\nCLASS:PUBLIC\r\nURL:http:\/\/[a-z0-9\.]+\/events\/234\r\nSTATUS:TENTATIVE\r\nSEQUENCE:1\r\nEND:VEVENT(\r\n)?\z/
+    expect( rendered ).to match(
+    /\ABEGIN:VEVENT\r\nUID:123.event@wayground.ca\r\nCREATED:20010203T110506Z\r\nDTSTAMP:20010203T040506Z\r\nDTSTART;TZID=America\/Denver:20110203T040506\r\nSUMMARY:Event One\r\nCLASS:PUBLIC\r\nURL:http:\/\/[a-z0-9\.]+\/events\/123\r\nSTATUS:CANCELLED\r\nSEQUENCE:1\r\nEND:VEVENT\r\nBEGIN:VEVENT\r\nUID:234.event@wayground.ca\r\nCREATED:20010203T110506Z\r\nDTSTAMP:20010203T040506Z\r\nDTSTART;TZID=America\/Denver:20110203T040506\r\nSUMMARY:Event Two\r\nCLASS:PUBLIC\r\nURL:http:\/\/[a-z0-9\.]+\/events\/234\r\nSTATUS:TENTATIVE\r\nSEQUENCE:1\r\nEND:VEVENT(\r\n)?\z/
     )
   end
 end
--- spec/views/events/show.ics.erb_spec.rb
+++ spec/views/events/show.ics.erb_spec.rb
@@ -6,34 +6,34 @@ describe "events/show.ics.erb" do
     Event.delete(123)
     Version.delete_all
     @event = FactoryGirl.create(:event,
-      :id => 123,
-      :user => nil,
-      :start_at => Time.parse('2011-02-03 04:05:06 am MST'),
-      :end_at => Time.parse('2011-07-08 09:10:11 am MDT'),
-      :is_allday => false,
-      :is_draft => false,
-      :is_approved => true,
-      :is_wheelchair_accessible => false,
-      :is_adults_only => false,
-      :is_tentative => false,
-      :is_cancelled => false,
-      :is_featured => false,
-      :title => "A Title That Extends Beyond Seventy-Five Characters So We Can Test Line Folding",
-      :description => "A description that goes on, and on, so it can exceed two lines of seventy-five characters. This should serve as a good test of the line folding. At least, that’s what I hope.",
-      :content => "Content",
-      :organizer => "Organizer",
-      :organizer_url => "http://organizer.tld/",
-      :location => "Location",
-      :address => "Address",
-      :city => "City",
-      :province => "Province",
-      :country => "CA",
-      :location_url => "http://location.tld/",
-      :created_at => Time.parse('2001-02-03 04:05:06 am MST'),
-      :updated_at => Time.parse('2001-02-03 04:05:06 am UTC')
+      id: 123,
+      user: nil,
+      start_at: Time.parse('2011-02-03 04:05:06 am MST'),
+      end_at: Time.parse('2011-07-08 09:10:11 am MDT'),
+      is_allday: false,
+      is_draft: false,
+      is_approved: true,
+      is_wheelchair_accessible: false,
+      is_adults_only: false,
+      is_tentative: false,
+      is_cancelled: false,
+      is_featured: false,
+      title: "A Title That Extends Beyond Seventy-Five Characters So We Can Test Line Folding",
+      description: "A description that goes on, and on, so it can exceed two lines of seventy-five characters. This should serve as a good test of the line folding. At least, that’s what I hope.",
+      content: "Content",
+      organizer: "Organizer",
+      organizer_url: "http://organizer.tld/",
+      location: "Location",
+      address: "Address",
+      city: "City",
+      province: "Province",
+      country: "CA",
+      location_url: "http://location.tld/",
+      created_at: Time.parse('2001-02-03 04:05:06 am MST'),
+      updated_at: Time.parse('2001-02-03 04:05:06 am UTC')
     )
-    FactoryGirl.create(:external_link, :url => 'http://1st.external.link/', :item => @event)
-    FactoryGirl.create(:external_link, :url => 'http://2nd.external.link/', :item => @event)
+    FactoryGirl.create(:external_link, url: 'http://1st.external.link/', item: @event)
+    FactoryGirl.create(:external_link, url: 'http://2nd.external.link/', item: @event)
   end
   before(:each) do
     assign(:event, @event)
@@ -42,7 +42,7 @@ describe "events/show.ics.erb" do
   it "renders the expected text" do
     render
     rendered.should match(/\ABEGIN:VEVENT\r/)
-    rendered.should match(/^UID:123-event@wayground.ca\r/)
+    rendered.should match(/^UID:123.event@wayground.ca\r/)
     rendered.should match(/^CREATED:20010203T110506Z\r/)
     rendered.should match(/^DTSTAMP:20010203T040506Z\r/)
     rendered.should match(/^DTSTART;TZID=America\/Denver:20110203T040506\r/)
