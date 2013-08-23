882ded2a5d4a269f716b4bba112fa93b109581f9
Grant Neufeld <grant@grantneufeld.ca>
2013-08-22 23:21:16 -0600

    democracy: Ballots index view revised.
    Include display of candidates with the ballots.
    
    Use hCard microformat attributes.

--- spec/views/ballots/index.html.erb_spec.rb
+++ spec/views/ballots/index.html.erb_spec.rb
@@ -5,7 +5,7 @@ require 'election'
 describe "ballots/index.html.erb" do
   let(:level) { $level = Level.new(filename: 'lvl') }
   let(:office) do
-    $office = level.offices.build(filename: 'offc', name: 'Stub Name', filename: 'stub_filename')
+    $office = level.offices.build(filename: 'offc', name: 'Stub Office')
     $office.level = level
     $office
   end
@@ -20,18 +20,29 @@ describe "ballots/index.html.erb" do
     $ballot.office = office
     $ballot
   end
+  let(:candidate) do
+    $candidate = ballot.candidates.build(filename: 'cnd', name: 'Stub Candidate')
+  end
   
   before(:each) do
     assign(:level, level)
     assign(:election, election)
     ballot.stub(:to_param).and_return('abc')
     assign(:ballots, [ballot, ballot])
+    candidate
     render
   end
   it "should present a list of the ballots" do
-    assert_select 'ul' do
-      assert_select 'li', count: 2 do
-        assert_select 'a', href: '/levels/lvl/elections/elct/ballots/stub_filename', text: 'Stub Name'
+    assert_select 'h2' do
+      assert_select 'a', href: '/levels/lvl/elections/elct/ballots/offc', text: 'Stub Office'
+    end
+  end
+  it 'should list the candidates on the ballots' do
+    assert_select 'div.vcard' do
+      assert_select 'h3.fn', count: 2 do
+        assert_select('a.url', href: '/levels/lvl/elections/elct/ballots/offc/candidates/cnd',
+          text: 'Stub Candidate'
+        )
       end
     end
   end
