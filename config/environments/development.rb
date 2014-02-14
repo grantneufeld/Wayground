19846c644a8c91d6303cb55bb996d7dce68b5875
Grant Neufeld <grant@grantneufeld.ca>
2014-02-14 12:05:48 -0700

    [rails] tighten up rails includes for application.
    Rather than including the default `rails/all`, be specific about which rails modules are used. Currently, not using action_mailer.
    
    Based on:
    http://www.amberbit.com/blog/2014/2/14/putting-ruby-on-rails-on-a-diet/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,10 @@ Wayground::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  # urls in emails point to localhost under development
-  config.action_mailer.default_url_options = { host: 'http://0.0.0.0:3000' }
+  ## Don't care if the mailer can't send.
+  #config.action_mailer.raise_delivery_errors = false
+  ## urls in emails point to localhost under development
+  #config.action_mailer.default_url_options = { host: 'http://0.0.0.0:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
