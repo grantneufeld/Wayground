6a4199789c057975541017d77477c4ba5e7977d1
Grant Neufeld <grant@grantneufeld.ca>
2016-08-06 02:47:08 -0600

    [validator][preflight] Cleanup `app/validators` based on Rubocop report.

--- app/validators/filename_validator.rb
+++ app/validators/filename_validator.rb
@@ -3,7 +3,7 @@ require 'active_model/validator'
 # Validate that a given value is valid as a Wayground-style filename (with no extension).
 # Used in ActiveModel/ActiveRecord field validations.
 class FilenameValidator < ActiveModel::EachValidator
-  FILENAME_REQUIREMENTS_MESSAGE = 'must only be lower-case letters, numbers, dashes and underscores'
+  FILENAME_REQUIREMENTS_MESSAGE = 'must only be lower-case letters, numbers, dashes and underscores'.freeze
 
   def validate_each(record, attribute, value)
     unless value && value.match(/\A[a-z0-9_\-]+\z/)
