fc06da7401f9228328cdcb7b307b731e3868fccc
Grant Neufeld <grant@grantneufeld.ca>
2017-06-22 15:34:35 -0600

    [preflight][cleanup] cleanup helper and lib specs based on rubocop report.

--- spec/lib/url_cleaner_spec.rb
+++ spec/lib/url_cleaner_spec.rb
@@ -2,43 +2,42 @@ require 'rails_helper'
 require 'url_cleaner'
 
 describe UrlCleaner do
-
-  describe ".clean" do
-    it "should recognize Facebook urls" do
+  describe '.clean' do
+    it 'should recognize Facebook urls' do
       cleaned_url = UrlCleaner.clean('http://www.facebook.com/events/123/declines/?test=1')
       expect(cleaned_url).to eq 'https://www.facebook.com/events/123/'
     end
-    it "should recognize Twitter urls" do
+    it 'should recognize Twitter urls' do
       cleaned_url = UrlCleaner.clean('http://twitter.com/#!/wayground')
       expect(cleaned_url).to eq 'https://twitter.com/wayground'
     end
-    it "should pass through other urls" do
+    it 'should pass through other urls' do
       cleaned_url = UrlCleaner.clean('http://user@wayground.ca:80/path/?q=1')
       expect(cleaned_url).to eq 'http://user@wayground.ca:80/path/?q=1'
     end
-    it "should pass through blank strings" do
+    it 'should pass through blank strings' do
       expect(UrlCleaner.clean('')).to eq ''
     end
-    it "should pass through nils" do
+    it 'should pass through nils' do
       expect(UrlCleaner.clean(nil)).to eq nil
     end
   end
 
-  describe ".clean_facebook" do
-    it "should change http to https" do
+  describe '.clean_facebook' do
+    it 'should change http to https' do
       cleaned_url = UrlCleaner.clean_facebook(
         protocol: 'http', delimiter: '://', domain: 'facebook.com', path: '/'
       )
       expect(cleaned_url).to eq 'https://facebook.com/'
     end
-    it "should tighten up event urls" do
+    it 'should tighten up event urls' do
       cleaned_url = UrlCleaner.clean_facebook(
         protocol: 'http', delimiter: '://', domain: 'facebook.com',
         path: '/events/123/declines/', params: '?ignore=this'
       )
       expect(cleaned_url).to eq 'https://facebook.com/events/123/'
     end
-    it "should pass through weird urls" do
+    it 'should pass through weird urls' do
       cleaned_url = UrlCleaner.clean_facebook(
         protocol: 'webcal', delimiter: '://', domain: 'facebook.com',
         path: '/something/weird/', params: '?with=params'
@@ -47,19 +46,18 @@ describe UrlCleaner do
     end
   end
 
-  describe ".clean_twitter" do
-    it "should change http to https" do
+  describe '.clean_twitter' do
+    it 'should change http to https' do
       cleaned_url = UrlCleaner.clean_twitter(
         protocol: 'http', delimiter: '://', domain: 'twitter.com', path: '/'
       )
       expect(cleaned_url).to eq 'https://twitter.com/'
     end
-    it "should remove the hashbang" do
+    it 'should remove the hashbang' do
       cleaned_url = UrlCleaner.clean_twitter(
         protocol: 'https', delimiter: '://', domain: 'twitter.com', path: '/#!/wayground'
       )
       expect(cleaned_url).to eq 'https://twitter.com/wayground'
     end
   end
-
 end
