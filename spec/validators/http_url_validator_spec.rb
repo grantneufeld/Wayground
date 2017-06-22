786847032317f734fd67590683a12b561118cac0
Grant Neufeld <grant@grantneufeld.ca>
2017-06-22 16:21:27 -0600

    [preflight][cleanup] cleanup validator specs based on rubocop report.

--- spec/validators/http_url_validator_spec.rb
+++ spec/validators/http_url_validator_spec.rb
@@ -5,7 +5,7 @@ class SpecClassWithHttpUrl
   include ActiveModel::Validations
   attr_accessor :url
   validates :url, http_url: true
-  def initialize(params={})
+  def initialize(params = {})
     self.url = params[:url]
   end
 end
@@ -14,44 +14,43 @@ describe HttpUrlValidator do
   let(:url) { $url = 'http://url.tld/' }
   let(:item) { $item = SpecClassWithHttpUrl.new(url: url) }
 
-  context "with an http url" do
-    it "should be valid" do
-      expect( item.valid? ).to be_truthy
+  context 'with an http url' do
+    it 'should be valid' do
+      expect(item.valid?).to be_truthy
     end
   end
-  context "with an http url" do
+  context 'with an http url' do
     let(:url) { $url = 'https://url.tld/' }
-    it "should be valid" do
-      expect( item.valid? ).to be_truthy
+    it 'should be valid' do
+      expect(item.valid?).to be_truthy
     end
   end
-  context "with an url with just a domain and no path" do
+  context 'with an url with just a domain and no path' do
     let(:url) { $url = 'https://url.tld/' }
-    it "should be valid" do
-      expect( item.valid? ).to be_truthy
+    it 'should be valid' do
+      expect(item.valid?).to be_truthy
     end
   end
-  context "with an url with a port, path, params and hash" do
+  context 'with an url with a port, path, params and hash' do
     let(:url) { $url = 'https://url.tld:1234/a/path-with/file.ext?x=y&foo=bar#hash' }
-    it "should be valid" do
-      expect( item.valid? ).to be_truthy
+    it 'should be valid' do
+      expect(item.valid?).to be_truthy
     end
   end
   context 'with an url with a bunch of weird, but allowed, characters' do
     let(:url) { $url = 'https://url.tld/AZaz09+:.-/%26~_@?=&#' }
-    it "should be valid" do
-      expect( item.valid? ).to be_truthy
+    it 'should be valid' do
+      expect(item.valid?).to be_truthy
     end
   end
-  context "with an invalid url" do
+  context 'with an invalid url' do
     let(:url) { $url = 'invalid://url.tld/' }
-    it "should return false" do
-      expect( item.valid? ).to be_falsey
+    it 'should return false' do
+      expect(item.valid?).to be_falsey
     end
-    it "should report an error" do
+    it 'should report an error' do
       item.valid?
-      expect( item.errors[:url].present? ).to be_truthy
+      expect(item.errors[:url].present?).to be_truthy
     end
   end
-
 end
